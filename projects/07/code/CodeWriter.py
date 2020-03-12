96887c0b4515224dba18bd06511c7338b278ad1e
kakkey <kakkey1964@yahoo.co.jp>
2020-03-12 17:33:08 +0900

    implemented other Arithmetic operators

--- projects/07/code/CodeWriter.py
+++ projects/07/code/CodeWriter.py
@@ -2,7 +2,24 @@ import io
 import re
 
 ArithmeticDict={
-    'add':['@SP','A=M-1','D=M','@SP','AM=M-1','A=A-1','M=D+M']
+    'add': ['@SP','A=M-1','D=M','@SP','AM=M-1','A=A-1','M=D+M'],
+    'sub': ['@SP','A=M-1','D=M','@SP','AM=M-1','A=A-1','M=D-M','M=-M'],
+    'and': ['@SP','A=M-1','D=M','@SP','AM=M-1','A=A-1','M=D&M'],
+    'or' : ['@SP','A=M-1','D=M','@SP','AM=M-1','A=A-1','M=D|M'],
+    'not': ['@SP','A=M-1','M=!M'],
+    'neg': ['@SP','A=M-1','M=-M'],
+
+    'eq' : ['@SP','A=M-1','D=M','@SP','AM=M-1','A=A-1','D=D-M',
+            '@TrueX','D;JEQ','D=0','@CMPENDX','0;JMP','(TrueX)',
+            'D=-1','(CMPENDX)','@SP','A=M-1','M=D'],
+
+    'gt' : ['@SP','A=M-1','D=M','@SP','AM=M-1','A=A-1','D=D-M',
+            '@TrueX','D;JLT','D=0','@CMPENDX','0;JMP','(TrueX)',
+            'D=-1','(CMPENDX)','@SP','A=M-1','M=D'],
+
+    'lt' : ['@SP','A=M-1','D=M','@SP','AM=M-1','A=A-1','D=D-M',
+            '@TrueX','D;JGT','D=0','@CMPENDX','0;JMP','(TrueX)',
+            'D=-1','(CMPENDX)','@SP','A=M-1','M=D']
 }
 
 PushPopDict={
@@ -10,17 +27,25 @@ PushPopDict={
     'pushD':['@SP','A=M','M=D','@SP','M=M+1']
 }
 
-
 class CodeWriter:
 
     def __init__(self, inputfile):
         self.outfile = inputfile.split('.')[0] + '.asm'
         self.filestream = open(self.outfile, 'w')
+        self.cmpcount=0
 
     def setFileName(self, fileName):
         self.fileName=fileName
 
     def writeArithmetic(self, command):
+        if command in ['eq','gt','lt']:
+            self.cmpcount+=1
+            self.counter = 0
+            ArithmeticDict[command][7]='@True'+str(self.cmpcount)
+            ArithmeticDict[command][10]='@COMPEND'+str(self.cmpcount)
+            ArithmeticDict[command][12]='(True'+str(self.cmpcount)+')'
+            ArithmeticDict[command][14]='(COMPEND'+str(self.cmpcount)+')'
+
         for cmd in ArithmeticDict[command]:
             self.filestream.write(cmd+'\n')
         
